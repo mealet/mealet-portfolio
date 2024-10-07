0e6dba433c2facdb24d1e4b52807442340d8944a
mealet <jasonragonter@gmail.com>
2024-10-07 19:34:09 +0300

    projects & contacts page

---
+++ app/contacts/contact_card.tsx
@@ -0,0 +1,33 @@
+import contacts from "./contacts.ts";
+
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card"
+import { Image } from "@nextui-org/image"
+import { Divider } from "@nextui-org/divider"
+import { Link } from "@nextui-org/link"
+
+
+export default function ContactCard() {
+  const avatar_link = "https://avatars.githubusercontent.com/u/110933288?s=400&u=a2c5fd751318efea89ffcc6c5eedd2b37c10d288&v=4";
+
+  return (
+      <Card className="w-[330px] h-[180px] max-h-[200px]">
+        <CardHeader className="flex gap-3">
+          <Image src={avatar_link} height={50} width={50} radius="sm" />
+          <div className="flex flex-col">
+            <p className="text-md">Damir Stash</p>
+            <p className="text-small text-default-500">mealet</p>
+          </div>
+        </CardHeader>
+
+        <Divider />
+        
+        <CardBody>
+          <p>
+            Email - <Link href="mailto:workjason34@gmail.com">workjason34@gmail.com</Link> <br/>
+            Telegram - <Link href="https://mealet.t.me">@mealet</Link> <br/>
+            Github - <Link href="https://github.com/mealet/">github.com/mealet</Link>
+          </p>
+        </CardBody>
+      </Card>
+  )
+}
